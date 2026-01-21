55d78b182d2caefe430aff2b8a85e254a4b935e5
watpadepriyanka <watpadepriyanka@gmail.com>
2026-01-21 14:57:10 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/publicis/movie_booking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicis.movie_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
